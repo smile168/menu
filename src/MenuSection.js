2cbd3d00c7fb0ddf2adef35e9739e695eb580f80
jason <jasonli110168@gmail.com>
2021-05-23 23:18:24 -0400

    add menu item, try cath img

--- src/MenuSection.js
+++ src/MenuSection.js
@@ -3,6 +3,15 @@ import { useTranslation } from 'react-i18next';
 
 function MenuSection (props) {
     const { t, i18n } = useTranslation();
+    const findImg = (cat, itemName) => {
+        let img;
+        try {
+            img = require(`./images/${cat}/${itemName}.jpg`).default
+        } catch(error) {
+            img = require(`./images/alt.jpg`).default
+        }
+        return img;
+    }
     return (
         <div className="menu-section">
             {/* <div className="menu-section-header">{props.chinese}</div> */}
@@ -10,7 +19,7 @@ function MenuSection (props) {
                     item => 
                     <div className="menu-item">
                         <img className="item-picture" 
-                            src={require(`./images/${props.chinese}/${item.name}.jpg`).default}/>
+                            src={findImg(props.chinese, item.name)}/>
                         <div className="item-details">
                             <div>{t(item.name)}</div>
                             <div>{item.price}</div>
